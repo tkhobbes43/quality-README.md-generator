54a7afb7a463a80e5102b40b6b9df18c4a0d6e9d
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-01 21:46:39 -0500

    started working on questions variable

--- index.js
+++ index.js
@@ -7,7 +7,34 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-
+    {
+        type: 'input',
+        message: "What is your Github username?",
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: "Write a description of  your project.",
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: "Describe the steps required to install your project for the Installation section.",
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        mesage: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage',
+    },
+    {
+        
+    }
 ];
 
 // TODO: Create a function to write README file
