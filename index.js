e78823b9843d8d4ee060ca02f2edb3bc48e6a724
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-01 23:05:23 -0500

    added writeToFile function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 // fs needed to write and append files
 const fs = require('fs');
-
 // inquirer needed to prompt user using command line for inputs to create the README.md
 const inquirer = require('inquirer');
 
@@ -50,12 +49,19 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, err => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log('Your markdown file has been created.')
+//     });
+// }
 
 // TODO: Create a function to initialize app
 function init() {
-    questions()
+    
         .then((answers) => fs.writeFile('README.md', generateREADME(answers)))
         .then(() =>console.log('Successfully wrote to README.md'))
         .catch((err) => console.error(err));
