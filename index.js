448f333fc401558ee415eb0a4364248c1f7f79ba
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-01 20:32:44 -0500

    added fs and inquirer packages

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
-fs.writeFile('README.md', data, (err) =>
-    err ? console.error(err) : console.log('Success!')
-);
 
+
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then((answers) => fs.writeFile('README.md', generateREADME(answers)))
+        .then(() =>console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
