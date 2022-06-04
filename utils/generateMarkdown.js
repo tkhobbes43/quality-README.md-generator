31503a23dc49ea769a8c9bb14ddb16cee503251d
Tommy Lee <t.k.hobbes43@gmail.com>
2022-06-03 20:41:16 -0500

    my version of renderlicensesection function does all three of what starter code had want me to do in what was 3 separate functions so got rid of that code and updated comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,8 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns the license section of README
+// returns license badge based on which license is passed in
+// as well as returns the license link
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// 
 function renderLicenseSection(license) {
   if(license === 'MIT License') {
     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
